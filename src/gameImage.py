b9c1eb1fc77f96c58d4caf4a6294e069f2d69afa
aksalcido <aksalcid@uci.edu>
2021-05-26 04:15:14 -0700

    Updated GameImage directory and organization

--- src/gameImage.py
+++ src/gameImage.py
@@ -9,9 +9,16 @@ class GameImage():
             through all of the images inside the 'images' directory. This makes all of the
             images available from different directories. '''
         self.start_directory = os.getcwd()
-        self.image_directory = f'{self.start_directory}\\static\\images'
+
+        # Ran from src directory
+        if self.start_directory.split('\\')[-1] == "src":
+            self.image_directory = f'..\\static\\images'
+        # Ran from Pacman directory
+        else:
+            self.image_directory = f'{self.start_directory}\\static\\images'
+
         self.game_images = dict()
-    
+
         for image in os.listdir( self.image_directory ):
             self.game_images[ image[:-4] ] = PhotoImage(file= f'{self.image_directory}\\{image}')
 
