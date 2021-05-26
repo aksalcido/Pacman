111a321c63fd130b9fc86433b19642e3b4205949
Armando Salcido <39717342+aksalcido@users.noreply.github.com>
2021-05-26 03:59:36 -0700

    Revert "Directory Cleanup/Organization"

---
+++ pickup.py
@@ -0,0 +1,18 @@
+class Pickup():
+    pickup = 1
+    boostUp = 3
+    
+    def __init__(self, x, y, images, boost = False):
+        ''' Initializes a Pickup object that basically functions as a representation on the
+            board. There is no methods in the class, because it gets eaten by Pacman as the
+            game progresses. Due to this, it requires x and y coordinates, an image, and the
+            difference between a boost pick up or a normal one, where boost is False by default.'''
+        self.x = x
+        self.y = y
+        self.images = images
+        self.boost = boost # default = false since only 4 boost are on the board
+        
+        if self.boost:
+            self._image = self.images.return_image('boost')
+        else:
+            self._image = self.images.return_image('pickup')
